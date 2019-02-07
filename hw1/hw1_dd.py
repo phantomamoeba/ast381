12c71362f7c0abbabb0a36fd07e23708e706ccf5
phantomamoeba <polonius@davistx.com>
2019-02-07 10:50:24 -0600

    initial

---
+++ hw1/hw1_dd.py
@@ -0,0 +1,34 @@
+#Dustin Davis
+#AST 381 Computational Astrophysics
+#Homework #1
+#Februrary 14, 2019
+
+__author__ = "Dustin Davis"
+
+
+PERIEHLION = 8766107800000. #cm
+APHELION = 524823895700000. #cm
+SEMI_MAJOR_AXIS = 266795001700000. #cm
+PERIOD = 27509.1291 * 86400. #days * seconds
+ECCENTRICITY = 0.96714291
+
+#don't care about inclination, etc for this exercise
+
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
