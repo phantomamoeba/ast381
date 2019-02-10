74d675bce8b78e09e15a5b518a2b12b35e3d1f51
phantomamoeba <polonius@davistx.com>
2019-02-10 11:03:36 -0600

    hw1

--- hw1/hw1_dd.py
+++ hw1/hw1_dd.py
@@ -11,6 +11,10 @@ I will ignore orbital angular momentum (noting that it is a conserved quantity) 
 since they are in a simpler form and the basis vectors remain constant (though polar coordinates are often a natural
 choice for central potentials) (and the usual GR stuff about being sufficiently far and in a weak field and not worry
 about curved metrics, etc).
+
+Also, using numerical pi, so there are small precision errors tha will creep in, but I am ignoring those as well.
+
+Coordinate axis places the Sun at 0,0
 """
 
 import numpy as np
@@ -18,7 +22,12 @@ import matplotlib.pyplot as plt
 
 PERIEHLION = 8766107800000. #cm
 APHELION = 524823895700000. #cm
-APHELION_VELOCITY = 90000.0 #cm/s
+
+#note: we are sensitive to the initial velocity (coupled with the time step size) as to whether the orbit will be
+# bound or not as the error in energy increases
+PERIEHLION_VELOCITY = 5500000. #cm/s
+APHELION_VELOCITY = 100000.#90000.0 #cm/s
+
 SEMI_MAJOR_AXIS = 266795001700000. #cm
 PERIOD = 27509.1291 * 86400. #days * seconds
 ECCENTRICITY = 0.96714291
@@ -26,7 +35,33 @@ G = 6.674e-8 #cm3 g-1 s-2
 M_SUN = 1.989e33 #g
 M_COMET = 2.2e17 #g
 #don't care about inclination, etc for this exercise
+AU = 1.496e13
 
+
+
+def make_plots(x,y,k,u,t,title="",fn=None):
+    plt.figure(figsize=(12,4))
+
+    plt.suptitle(title)
+    plt.subplot(121)
+    plt.title("Position")
+    plt.ylabel("[AU]")
+    plt.xlabel("[AU]")
+    plt.scatter(0,0,marker='.',s=50,c='orange')
+    plt.plot(x/AU,y/AU)
+
+
+    plt.subplot(122)
+    plt.title("Total Energy")
+    plt.ylabel(r"$erg\ \times10^{29}$")
+    plt.xlabel("Time [PERIOD]")
+    plt.plot(t/PERIOD,(k+u)/1e29)
+
+    if fn is not None:
+        plt.savefig(fn)
+    else:
+        plt.show()
+    plt.close('all')
 
 def cart_distance(x1,y1,x2=0,y2=0):
     return np.sqrt((x1-x2)**2 + (y1-y2)**2)
@@ -47,7 +82,39 @@ def kinetic_energy(vx,vy):
     return 0.5 * M_COMET * (vx**2 + vy**2)
 
 
-def explicit_euler(x=[APHELION],y=[0],vx=[0],vy=[APHELION_VELOCITY],dt=[PERIOD/1000.],orbits=10):
+def accel(x,y):
+    """
+    From Gravitational force only, always pointing toward the origin
+
+    Just to be explicit, F/m = a ... so this is the same calculation as force, but w/o the comet mass
+
+    :param x:
+    :param y:
+    :return:
+    """
+
+    theta = np.arctan2(x, y) - np.pi / 2 #-pi/2 so the 0 angle is along the +x axis
+    f = -G * M_SUN  / (cart_distance(x, y) ** 2)
+    ax = f * np.cos(theta)
+    ay = -1. * f * np.sin(theta)
+
+    return ax,ay
+
+def time_step(x, y, adaptive=False):
+    """
+
+    :param x:
+    :param y:
+    :param adaptive:
+    :return:
+    """
+    if adaptive:
+        pass
+    else:
+        return PERIOD/1000.
+
+
+def explicit_euler(x=[APHELION],y=[0],vx=[0],vy=[APHELION_VELOCITY],orbits=10, adaptive_time = False):
     """
 
     :param x: vector of x positions, length 1 s|t x[0] == initial x position
@@ -58,12 +125,67 @@ def explicit_euler(x=[APHELION],y=[0],vx=[0],vy=[APHELION_VELOCITY],dt=[PERIOD/1
     :param orbits: integer number of orbits to simulate
     :return: position vector (x,y,t) as 2D matrix
     """
-    pass
-    #todo: define sub steps here
+    def next_velocity(x,y,vx,vy,dt):
+        """
 
+        :param x,y: current (nth) position
+        :param vx,xy: current (nth) velocity
+        :param dt:timestep
+        :return: n+1 velocity in x and y
+        """
+
+        ax,ay = accel(x,y)
+        next_vx = vx + dt*ax
+        next_vy = vy + dt*ay
+
+        return next_vx, next_vy
+
+    def next_position(x,y,vx,vy,dt):
+        """
+
+        :param x:
+        :param y:
+        :param vx:
+        :param vy:
+        :param dt:
+        :return:
+        """
+
+        next_vx, next_vy = next_velocity(x,y,vx,vy,dt)
+        next_x = x + dt*next_vx
+        next_y = y + dt*next_vy
+
+        return next_x, next_y, next_vx, next_vy
+
+
+
+
+
+    n = 0 #index
+    time = [0.]
+    while time[n] < orbits * PERIOD:
+        dt = time_step(x[n],y[n],adaptive_time)
+        time.append(time[n]+dt)
+        next_x, next_y, next_vx, next_vy = next_position(x[n],y[n],vx[n],vy[n],dt)
+        x.append(next_x)
+        y.append(next_y)
+        vx.append(next_vx)
+        vy.append(next_vy)
+        n+=1
+
+    return np.array(x), np.array(y), np.array(vx), np.array(vy), np.array(time)
 
 def main():
-    pass
+
+
+    #first explicit Euler
+    x,y,vx,vy,t = explicit_euler(orbits=10.) #use all default values
+    u = potential_energy(x,y)
+    k = kinetic_energy(vx,vy)
+
+    make_plots(x,y,k,u,t,"Explicit Euler")
+
+
 
 if __name__ == '__main__':
     main()
